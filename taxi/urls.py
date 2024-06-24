012ae65f47aaef493d78daa02e78b46bb5d6273a
Artem Yatsuta <yatsutaproject@gmail.com>
2024-06-24 12:30:18 +0200

    long lines

--- taxi/urls.py
+++ taxi/urls.py
@@ -47,15 +47,21 @@ urlpatterns = [
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path(
-        "cars/<int:pk>/assign/", AssignUserToCarView.as_view(), name="car-assign"
+        "cars/<int:pk>/assign/",
+        AssignUserToCarView.as_view(),
+        name="car-assign"
     ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
     path(
-        "drivers/<int:pk>/update/", DriverUpdateView.as_view(), name="driver-update"
+        "drivers/<int:pk>/update/",
+        DriverUpdateView.as_view(),
+        name="driver-update"
     ),
     path(
-        "drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
     ),
 
     path(
